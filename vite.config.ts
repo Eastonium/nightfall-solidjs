eb646569a8b72c8b45543e9e36711042226837d2
Eastonium <9437330+Eastonium@users.noreply.github.com>
2024-01-11 17:03:24 -0800

    Updated packages trying to get devtools to work But the extension doesn't work in edge... big RIP

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
 import devtools from "solid-devtools/vite";
-import solidPlugin from "vite-plugin-solid";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [devtools({ autoname: true }), solidPlugin(), tsconfigPaths()],
+	plugins: [
+		devtools({
+			autoname: true,
+			locator: {
+				targetIDE: "vscode",
+				key: "Ctrl",
+				jsxLocation: true,
+				componentLocation: true,
+			},
+		}),
+		solid({ hot: false }),
+		tsconfigPaths(),
+	],
 	build: {
 		target: "esnext",
 	},
